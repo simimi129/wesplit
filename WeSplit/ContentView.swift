fad3d75fc8ffb9f574f9c8d6b45e80606a189424
simonszabo <simimi129@gmail.com>
2022-10-26 22:08:23 +0200

    adding tip% selector

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -2,20 +2,38 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var checkAmount = 0.2
-  @State private var numberOfPeople = 02
+  @State private var numberOfPeople = 2
   @State private var tipPercentage = 20
   
   let tipPercentages = [10, 15, 20, 25, 0]
   
   var body: some View {
-    Form {
-      Section {
-        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-          .keyboardType(.decimalPad)
+    NavigationView {
+      Form {
+        Section {
+          TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+            .keyboardType(.decimalPad)
+          Picker("number of people", selection: $numberOfPeople) {
+            ForEach(2..<100) {
+              Text("\($0) people")
+            }
+          }
+        }
+        Section {
+          Picker("Tip percentage", selection: $tipPercentage) {
+            ForEach(tipPercentages, id: \.self) {
+              Text($0, format: .percent)
+            }
+          }
+          .pickerStyle(.segmented)
+        } header: {
+          Text("How much tip do you want to leave?")
+        }
+        Section {
+          Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+        }
       }
-      Section {
-        Text(checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-      }
+      .navigationTitle("WeSplit")
     }
   }
 }
