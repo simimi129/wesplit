04783fdd7dfb69a72f8f33561d97ab9b66694c90
simonszabo <simimi129@gmail.com>
2022-10-26 20:59:18 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Simon Szabo on 2022. 10. 26..
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
