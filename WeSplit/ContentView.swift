80293aee2c4d0c57d29b8315d94217ac836630c8
simonszabo <simimi129@gmail.com>
2022-10-26 21:12:37 +0200

    navView

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,26 +1,25 @@
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by Simon Szabo on 2022. 10. 26..
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          Text("Hi")
+          Text("Hi")
         }
-        .padding()
+        Section {
+          Text("Hi")
+        }
+      }
+      .navigationTitle("SwiftUI")
+      .navigationBarTitleDisplayMode(.inline)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- WeSplit/WeSplitApp.swift
+++ WeSplit/WeSplitApp.swift
@@ -1,17 +1,10 @@
-//
-//  WeSplitApp.swift
-//  WeSplit
-//
-//  Created by Simon Szabo on 2022. 10. 26..
-//
-
 import SwiftUI
 
 @main
 struct WeSplitApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
