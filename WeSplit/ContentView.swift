d64d7369a3f91e7486f50890a1fa876facd5a2b4
simonszabo <simimi129@gmail.com>
2022-10-26 21:19:44 +0200

    form 2way binding

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,11 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var tapCount = 0
+  @State private var name = ""
   
   var body: some View {
-    Button("Tap Count: \(tapCount)") {
-      tapCount += 1
+    Form {
+      TextField("Enter your name", text: $name)
+      Text("Your name is \(name)")
     }
   }
 }
